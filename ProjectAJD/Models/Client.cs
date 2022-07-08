3459b2178adee90632ee8f22b006fa1f23d12864
Arkajyoti Deb <arka.deb@geotechinfo.net>
2022-07-08 14:49:15 +0530

    Client Registration

---
+++ ProjectAJD/Models/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace ProjectAJD.Models
+{
+    public class Client
+    {
+        [Key]
+        public virtual long Id { get; set; }
+        [Required(ErrorMessage = "All the fields are required")]
+        public virtual string Name { get; set; }
+        [Required(ErrorMessage = "All the fields are required")]
+        public virtual string Age { get; set; }
+        [Required(ErrorMessage = "All the fields are required")]
+        public virtual string Gender { get; set; }
+    }
+
+    public class EFProjectAJDEntities : DbContext
+    {
+        public DbSet<Client> clients { get; set; }
+    }
+}
